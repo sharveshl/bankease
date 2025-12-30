2edb7688c8db42b60df6ae47e488924681bd77d3
sharveshl <loganathansharvesh14@gmail.com>
2025-12-30 11:13:51 +0530

    first commit

---
+++ src/test/java/com/bankese/AppTest.java
@@ -0,0 +1,20 @@
+package com.bankese;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
